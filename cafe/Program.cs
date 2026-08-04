a9c3584864eab38df4ee5f177f4d05c6ae63c7dc
LAP235-jpg <lapa86002@email.com>
2026-08-04 10:33:21 -0300

    first commit

---
+++ cafe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cafe;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var app = new CafeApp();
+        app.Run();
+    }
+}
+
+public class CafeApp
+{
+    public void Run()
+    {
+        Console.WriteLine("Welcome to the Cafe!");
+        Console.WriteLine("This is a basic C# console app structure.");
+    }
+}
+
