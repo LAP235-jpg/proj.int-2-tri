764c57bf4d5dc123560e8095c9c9b45aac944b1b
LAP235-jpg <lapa86002@gmail.com>
2026-08-19 10:49:34 -0300

    update

--- IA/app.py
+++ IA/app.py
@@ -1,16 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
+
+
+# Guarda a última temperatura recebida
+ultima_temperatura = 0
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
 
+    global ultima_temperatura
+
     dados = request.get_json()
 
     temperatura = dados["temperatura"]
+
+    ultima_temperatura = temperatura
 
     print(f"Temperatura recebida: {temperatura}")
 
@@ -24,9 +33,13 @@ def predict():
 def temperatura():
 
     return jsonify({
-        "temperatura": 30
+        "temperatura": ultima_temperatura
     })
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True
+    )
